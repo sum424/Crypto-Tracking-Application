3f71160357f7d3dba63243bbe261493e20d32b25
sum424 <sumanthapa326@gmail.com>
2024-12-07 19:30:20 -0500

    added pages components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import Home from './assets/components/pages/Home.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -27,6 +27,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      < Home />
     </div>
   )
 }
---
+++ src/assets/components/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function() {
+    return (
+        <div>
+            <h1>hello world from home</h1>
+        </div>
+    )
+}
+
